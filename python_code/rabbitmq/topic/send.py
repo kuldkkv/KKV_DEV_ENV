553c80912938c4937e37584b52e5196bb155a723
Kuldeep Verma <kuld.kkv@gmail.com>
2021-05-13 00:37:09 +0530

    added mq topic

---
+++ python_code/rabbitmq/topic/send.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import pika
+import sys
+import datetime
+
+conn = pika.BlockingConnection(pika.ConnectionParameters('centos'))
+channel = conn.channel()
+
+channel.exchange_declare(exchange = 'fa_notif_topic', exchange_type = 'topic')
+
+#message = ' '.join(sys.argv[1:]) or 'Hello World!'
+
+entity = sys.argv[1] if len(sys.argv) > 1 else '<ERROR>'
+
+message = entity + ' ready at ' + str(datetime.datetime.now())
+
+channel.basic_publish(exchange='fa_notif_topic',
+                      routing_key=entity,
+                      body=message
+                      )
+
+print("message %r sent for %r" % (message, entity))
+
+conn.close()
